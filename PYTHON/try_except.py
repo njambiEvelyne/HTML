75aff891cb8458850dba101b328209a420c6bcb5
njambiEvelyne <evelynenganga76@gmail.com>
2026-06-27 12:30:59 +0300

    Saved the value eror as a literal variable

--- PYTHON/try_except.py
+++ PYTHON/try_except.py
@@ -1,9 +1,9 @@
 try:
-    value = 10/0
+    #value = 10/0
     number = int(input("Enter a number: "))
     print(number)
 except ZeroDivisionError as err:
     print(err)
-except ValueError:
-    print("Invalid Input")
+except ValueError as e:
+    print(e)
 
