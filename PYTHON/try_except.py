5c59e94cc06f3f3cfcad7e3836823a884e3754c4
njambiEvelyne <evelynenganga76@gmail.com>
2026-06-27 12:27:45 +0300

    Added exceptiond for zero division error and value errors

--- PYTHON/try_except.py
+++ PYTHON/try_except.py
@@ -1,6 +1,9 @@
 try:
-  value = 10/0
-  number = int(input("Enter a number: "))
-  print(number)
-except:
-  print("Invalid input!")
+    value = 10/0
+    number = int(input("Enter a number: "))
+    print(number)
+except ZeroDivisionError:
+    print("Divided by zero")
+except ValueError:
+    print("Invalid Input")
+
