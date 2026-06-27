0e0d97c99c6d076a4c1fee7f0021d247f54e5fac
njambiEvelyne <evelynenganga76@gmail.com>
2026-06-27 13:41:04 +0300

    Added an append function to add on to the external file

--- PYTHON/try_except.py
+++ PYTHON/try_except.py
@@ -8,8 +8,10 @@
 #     print(e)
 
 #Reading from external files
-employee_file = open("employees.txt", "r")
-print(employee_file.readlines())
-employee_file.close()
+# employee_file = open("employees.txt", "r")
+# print(employee_file.readlines())
+# employee_file.close()
  
- 
+#Writing a file
+employee_file = open("employees.txt", "a")
+employee_file.write("Evelyne - Human Resources")
