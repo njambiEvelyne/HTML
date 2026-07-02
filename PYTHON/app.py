207e3c3e152c21a5a6ddc7dbe5539c3724cb21cc
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-02 10:10:18 +0300

    Added attributes for the class

--- PYTHON/app.py
+++ PYTHON/app.py
@@ -4,5 +4,10 @@
 
 # CLASSES AND OBJECTS
 class student:
-  def __init__(self, name):
-    pass
+  def __init__(self, name,major, gpa, is_on_probation):
+    self.name = name
+    self.major = major
+    self.gpa= gpa
+    self.is_on_probation = is_on_probation
+    
+    
