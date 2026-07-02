b629dbc3ad6cf4e7ab026f36a1feb091c3870ef6
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-02 10:08:11 +0300

    Created a class

--- PYTHON/app.py
+++ PYTHON/app.py
@@ -1,3 +1,8 @@
-import usefull_tools
-print (usefull_tools.beatles)
-print(usefull_tools.roll_dice(10))  
+# import usefull_tools
+# print (usefull_tools.beatles)
+# print(usefull_tools.roll_dice(10))  
+
+# CLASSES AND OBJECTS
+class student:
+  def __init__(self, name):
+    pass
