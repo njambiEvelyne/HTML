20ad5945752342daa2e37461d694d8a776e7a872
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-02 10:43:08 +0300

    Added arrays for the question prompts

--- PYTHON/app.py
+++ PYTHON/app.py
@@ -13,8 +13,22 @@ class student:
 student1 = student("Evelyne", "SOEN", 12.90, False)
 #print(student1.gpa)
 
-question_prompt=[
+class Question:
+  def __init__(self, prompt, answer):
+    self.propmt = prompt
+    self.answer= answer
+
+     
+
+question_prompts=[
   "What color are apples? \n(a) Red/Green\n(b) Purple\n(c) Orange \n\n",
   "What color are Bananas?\n(a) Teal\n(b)Magenta\n(c) Yellow\n\n",
   "What color are starwberries?\n(a) Yellow\n(b) Red\n(c)Blue\n\n"
 ]
+questions = [
+  Question(question_prompts[0], "a"),
+  Question(question_prompts[1], "c"),
+  Question(question_prompts[2], "b"),
+
+]
+
