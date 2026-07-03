46cccf386226b87d2127d10f33d6f3cd5d533a0e
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-03 17:30:34 +0300

    Added a service function for the Student class

--- PYTHON/class_functions.py
+++ PYTHON/class_functions.py
@@ -3,3 +3,10 @@ class Student:
     self.name = name
     self.major = major
     self.gpa = gpa
+
+  def on_honor_role(self):
+    if self.gpa >= 3.5:
+      return True
+    else:
+      return False
+
