56317f72e49b1f0f859281aa622b7cd51f0c7e33
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-03 17:28:20 +0300

    Created objects from the student class

---
+++ PYTHON/app2.py
@@ -0,0 +1,4 @@
+from class_functions import Student
+student1 = Student("Oscar", "Accounting", 3.1)
+student2 =  Student("Evelyne", "Software Enginnering", 4.5)
+student3 = Student("Rose", "Electrician", 4.1)
