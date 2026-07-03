4395bed47661ebc12333a177f9b32241f48dfbad
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-03 17:00:05 +0300

    Added a function to display the output for the correct questions

--- PYTHON/app.py
+++ PYTHON/app.py
@@ -36,6 +36,8 @@ def run_test(questions):
     answer = input(question.prompt)
     if answer == question.answer:
       score +=1
+  print("You got " + str(score) + "/" + str(len(questions)) +"correct")
+
 
 
 
