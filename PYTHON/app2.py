20f59e30c4b4188288518aebf600ce8063461aa6
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-03 17:37:38 +0300

    Added a printout statement for all student objetcs

--- PYTHON/app2.py
+++ PYTHON/app2.py
@@ -3,4 +3,4 @@ student1 = Student("Oscar", "Accounting", 3.1)
 student2 =  Student("Evelyne", "Software Enginnering", 4.5)
 student3 = Student("Rose", "Electrician", 4.1)
 
-print(student1.on_honor_role()) 
+print(student1.on_honor_role(), student2.on_honor_role(), student3.on_honor_role()) 
