55c7cad8205114f7e75e390e6680889ef85a7053
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-04 19:24:30 +0300

    Inserted elements into the list

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -1,5 +1,8 @@
 names = ["Evelyne", "Njambi", "Peter", "Rosemary", "Victor"]
 mode_of_transportation = ["Road", "Air", "Railway", "Water"]
+names.append("Elizabeth")
+names.insert(0, "Peter")
+
 for name in names:
   for mode in mode_of_transportation:
       print(f"Hello {name}. You love using {mode} for travelling.")
