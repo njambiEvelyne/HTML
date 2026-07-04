9ae48647f59c13f5e9d2a4a28181167f4c4fa373
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-04 20:33:43 +0300

    Added a range for the comprehension list

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -59,4 +59,9 @@ for value in range(1,10):
    square = value **2
    squares.append(square)
 print(squares)
+print()
+
+print("Results from a list comprehension:")
+squares2 = [value **2 for value in range(1,13)]
+print(squares2)
 
