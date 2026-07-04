1aeac0f66e1d49a341d97d82e5b1b6a39a11e68c
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-04 19:15:25 +0300

    Printed a message for all membres in the list

---
+++ PYTHON/py2/prac.py
@@ -0,0 +1,8 @@
+names = ["Evelyne", "Njambi", "Peter", "Rosemary", "Victor"]
+for name in names:
+  print(f"Hello {name}")
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[4])
