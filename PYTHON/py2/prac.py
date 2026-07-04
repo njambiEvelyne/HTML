2a4b8e3b0b2769b4a6fa4bdb91392419c7b5c67c
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-04 20:28:44 +0300

    Printed out the squares for numbers

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -52,5 +52,11 @@ print()
 print("Even numbers")
 even_numbers = list(range(0,11,2))
 print(even_numbers)
+print()
 
+squares = []
+for value in range(1,10):
+   square = value **2
+   squares.append(square)
+print(squares)
 
