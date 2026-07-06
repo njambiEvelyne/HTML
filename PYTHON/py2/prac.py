8d5797efbd1067bfcff42acc4537fd2bf199b62f
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-06 19:26:57 +0300

    Added a modification for the elements in the dictionary

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -234,7 +234,8 @@ alien_0={
 }
 print(alien_0)
 print(alien_0["color"])
-print("Adding key-value pairs ina dictionary")
+print("Adding key-value pairs in a dictionary")
 alien_0["x-position"] =0
 alien_0['y-position']= 25
 print(alien_0)
+print("Changing the values of a dictionary")
