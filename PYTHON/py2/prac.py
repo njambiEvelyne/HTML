741638e7de0c40b4a1e3ae3f144724395826e134
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-06 19:06:06 +0300

    Primted ot=ut the numbers ending drom 1-9

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -222,4 +222,8 @@ for number in ordinal_list:
       print("1st")
    elif number ==2 :
       print("2nd")
-   
+   elif number ==3:
+      print("3rd")   
+   else: 
+      print(f"{number}th")
+
