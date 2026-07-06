9b84beceacf79a51392c55d6c3ba7610702215fe
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-06 19:39:38 +0300

    Moved the alien position on the x-axis

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -247,3 +247,12 @@ print(alien_0)
 print(f"Original x-position: {str(alien_0['x-position'])}")
 #Move alien to the right
 #Determine how far to move the alien based on its current speed
+if alien_0["speed"]== "slow":
+   x_increment = 1
+elif alien_0["speed"] =="medium":
+   x_increment = 2
+else:
+   x_increment = 3
+#The new position plus the increment
+alien_0["x-position"] = alien_0["x-position"] + x_increment
+print(f"New postion: {str(alien_0['x-position'])}")
