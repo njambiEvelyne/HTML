12c358c55ca4e9d6cec49c1e9fc3f2b0db5d6a2b
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-07 18:32:46 +0300

    Nested all dictionaries to to a single variable

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -303,3 +303,6 @@ print("Nesting dictionaries")
 alien_1= {'color': 'green', 'points': 5}
 alien_2 = {'color': 'yellow', 'points': 10}
 alien_3 = {'color': 'red', 'points': 15}
+aliens = [alien_1, alien_2, alien_3]
+for alien in aliens:
+ print(alien)
