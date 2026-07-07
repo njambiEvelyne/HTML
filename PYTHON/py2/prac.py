d5ad40fb016d7a6ba5520dc4b12c6f07f3cbf54b
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-07 18:45:31 +0300

    Modified the first three dictionaries and left the rest untouched

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -310,13 +310,16 @@ for alien in aliens:
 print()
 aliens = []
 # Make 30 green aliens.
-for alien_number in range(30):
+for alien_number in range(0,30):
   new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
   aliens.append(new_alien)
 
 # Show the first 5 aliens:
-for alien in aliens[:5]:
-  print(alien)
+for alien in aliens[0:3]:
+   if alien["color"]=="green":
+      alien["color"]= "yellow"
+      alien["speed"]= "medium"
+      alien["points"]= 10
+for alien in aliens[0:5]:
+   print(alien)
 print("...")
-# Show how many aliens have been created.
-print("Total number of aliens: " + str(len(aliens)))
