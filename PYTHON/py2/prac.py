b0c2b41ed4b32939c5c1df5ff3fdf43ce8c2c644
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-12 21:55:06 +0300

    Appended the confirmed users list

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -437,4 +437,4 @@ confirmed_users = []
 while unconfirmed_users:
    current_user = unconfirmed_users.pop()
    print(f"Veryfying user: {current_user.title()}")
-   
+   confirmed_users.append(current_user)
