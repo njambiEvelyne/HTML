ec760c17dab0f9dd8e4b50c6992de798692dcf4a
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-13 21:52:29 +0300

    Added a comment

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -438,3 +438,4 @@ while unconfirmed_users:
    current_user = unconfirmed_users.pop()
    print(f"Veryfying user: {current_user.title()}")
    confirmed_users.append(current_user)
+#Display all confirmed users
