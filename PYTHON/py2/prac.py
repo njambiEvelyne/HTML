a7c7cb4d415f1627151402cb4fc61981c190c30a
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-16 21:58:35 +0300

    Commented out height code

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -398,12 +398,12 @@ for username, user_info in users4.items():
 #    food_detail = details["cakes"] + " " + details["pizza"]
 
 #User input and loops
-height = input("How tall are you in inches? ")
-height = int(height)
-if height >=36:
-   print("\nYou are tall enough to ride!")
-else:
-   print("\n You will be able to ride when you become a littel bit taller!")
+# height = input("How tall are you in inches? ")
+# height = int(height)
+# if height >=36:
+#    print("\nYou are tall enough to ride!")
+# else:
+#    print("\n You will be able to ride when you become a littel bit taller!")
 current_number= 0
 while current_number<=5:
    print(current_number)
