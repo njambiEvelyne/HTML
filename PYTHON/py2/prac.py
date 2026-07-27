050cb8e30030e90dd3acee33099927e51cab370a
njambiEvelyne <evelynenganga76@gmail.com>
2026-07-27 20:42:58 +0300

    Added a name variable

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -512,6 +512,6 @@ def get_formatted_name2(first_name, last_name):
  return full_name.title()
 
 while True: 
-   print("\nPlease tell ")
+   print("\nPlease tell me our name:")
    print("(Eneter 'q' at any time to quit)")
    
