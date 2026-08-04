4fe75f85220049c7b9b3acdf58bdbbcd4c474185
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-04 20:24:42 +0300

    Used the title method for the contents print out

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -594,5 +594,5 @@ def make_pizza_summarise(*topping3):
       print("-" + topppin4)
 
 make_pizza_summarise("pepperoni")
-make_pizza_summarise("musgrooms", "green peppers", "extra cheese")
+make_pizza_summarise("mushrooms", "green peppers", "extra cheese")
 
