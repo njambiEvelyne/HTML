247702f947d97f7959dbcb81a844e9de4729d46e
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-05 19:12:43 +0300

    Added a comment for the summary of tops

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -600,5 +600,5 @@ def make_pzz(size, *tops):
    """
    Summarise the pizza we are about to make
    """
-   print("\nMaking a " + str(size) + "-inch")
+   print("\nMaking a " + str(size) + "-inch pizza with the following toppings: ")
 
