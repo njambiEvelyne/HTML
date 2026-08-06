c0cbada9f9fcdf357eb2c1a669e23bb8cd10e1c0
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-06 19:19:06 +0300

    Added a user profile object

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -617,6 +617,7 @@ def build_profile(first, last, *user_info):
    for key, value in profile.items():
       profile[key] = value
    return profile
+user_profile = build_profile("albert", "einstein", location = "princeton", field = )
 
 
 
