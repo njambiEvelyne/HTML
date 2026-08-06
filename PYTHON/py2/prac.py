1cd01196fe653da05446592d6524ea3b7a3b5e6f
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-06 19:16:15 +0300

    Pointed the key-values pairs in the dictionary

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -615,7 +615,7 @@ def build_profile(first, last, *user_info):
    profile['first_name'] = first
    profile['last_name']= last
    for key, value in profile.items():
-      
+      profile[key] = value
 
 
 
