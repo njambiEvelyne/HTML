c0280a633be7a5735037aa6514aaf1f163ca0492
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-06 19:21:31 +0300

    Printed the user profile object

--- PYTHON/py2/prac.py
+++ PYTHON/py2/prac.py
@@ -618,6 +618,7 @@ def build_profile(first, last, *user_info):
       profile[key] = value
    return profile
 user_profile = build_profile("albert", "einstein", location = "princeton", field = "physics")
+print(user_profile)
 
 
 
