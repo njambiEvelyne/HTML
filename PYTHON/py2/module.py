f0a5b60811e852fd0e9e216aa26c6b3aa384e47f
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-10 22:33:20 +0300

    Commented out code

--- PYTHON/py2/module.py
+++ PYTHON/py2/module.py
@@ -1,4 +1,4 @@
-from prac import make_pzz
-make_pzz.pizza(16, "mushroom")
-print(make_pzz)
-make_pzz.pizza2(12, "mushrooms", "green peppers")
+# from prac import make_pzz
+# make_pzz.pizza(16, "mushroom")
+# print(make_pzz)
+# make_pzz.pizza2(12, "mushrooms", "green peppers")
