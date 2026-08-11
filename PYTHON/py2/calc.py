b81a80e8abdfb8a3aac3d40da58f80bb88a265ac
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-11 21:18:48 +0300

    Added an if loop to check for the choice for the operator type

--- PYTHON/py2/calc.py
+++ PYTHON/py2/calc.py
@@ -1,3 +1,7 @@
 num1 = float(input("Enter the first number: "))
 operator = input("Enter the operator('+', '-', '/', '*')")
 num2 = float(input("Enter the second number: "))
+if operator == "+":
+  result = num1 + num2
+elif operator == "-":
+  result = num1 - num2
