33d9f51157ef9325ed8cb3ec028b9e0b135d10be
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-15 23:52:23 +0300

    Edited the class_function file

--- PYTHON/class_functions.py
+++ PYTHON/class_functions.py
@@ -2,7 +2,7 @@ class Student:
   def __init__(self, name, major, gpa):
     self.name = name
     self.major = major
-    self.gpa = gp
+    self.gpa = gpa
 
   def on_honor_role(self):
     if self.gpa >= 3.5:
