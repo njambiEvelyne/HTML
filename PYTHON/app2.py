542cf78d7ced0cb9a34dc28f187689cae57e8038
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-17 21:26:00 +0300

    Added a new variable

--- PYTHON/app2.py
+++ PYTHON/app2.py
@@ -2,5 +2,6 @@ from class_functions import Student
 student1 = Student("Oscar", "Accounting", 3.1)
 student2 =  Student("Evelyne", "Software Enginnering", 4.5)
 student3 = Student("Rose", "Electrician", 4.1)
+stude
 
 print(student1.on_honor_role(), student2.on_honor_role(), student3.on_honor_role()) 
