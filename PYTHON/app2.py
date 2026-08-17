66c7fa12de73a9acca29005724e6221f8a9649b0
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-17 21:27:25 +0300

    Called the imported fuction

--- PYTHON/app2.py
+++ PYTHON/app2.py
@@ -2,6 +2,6 @@ from class_functions import Student
 student1 = Student("Oscar", "Accounting", 3.1)
 student2 =  Student("Evelyne", "Software Enginnering", 4.5)
 student3 = Student("Rose", "Electrician", 4.1)
-stude
+student5 = Student()
 
 print(student1.on_honor_role(), student2.on_honor_role(), student3.on_honor_role()) 
