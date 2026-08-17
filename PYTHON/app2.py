34bbaa561958d68d67c903a0486c6f8c4540efd6
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-17 21:29:45 +0300

    Added the major time of pursuit

--- PYTHON/app2.py
+++ PYTHON/app2.py
@@ -2,6 +2,6 @@ from class_functions import Student
 student1 = Student("Oscar", "Accounting", 3.1)
 student2 =  Student("Evelyne", "Software Enginnering", 4.5)
 student3 = Student("Rose", "Electrician", 4.1)
-student5 = Student("Eva", "Software Engineer")
+student5 = Student("Eva", "Software Engineer", 3.1)
 
 print(student1.on_honor_role(), student2.on_honor_role(), student3.on_honor_role()) 
