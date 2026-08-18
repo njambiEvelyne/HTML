9c25bf18e98260e8a33915848c4622441fe6e1de
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-18 21:24:02 +0300

    Created a new object

--- PYTHON/app2.py
+++ PYTHON/app2.py
@@ -3,5 +3,6 @@ student1 = Student("Oscar", "Accounting", 3.1)
 student2 =  Student("Evelyne", "Software Enginnering", 4.5)
 student3 = Student("Rose", "Electrician", 4.1)
 student5 = Student("Eva", "Software Engineer", 3.1)
+student6 
 
 print(student1.on_honor_role(), student2.on_honor_role(), student3.on_honor_role()) 
