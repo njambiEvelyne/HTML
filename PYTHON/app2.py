dc8569ea06ffe699d9189424e98cf2d58a26c266
njambiEvelyne <evelynenganga76@gmail.com>
2026-08-18 21:30:07 +0300

    Added The year pursuit argument

--- PYTHON/app2.py
+++ PYTHON/app2.py
@@ -3,6 +3,6 @@ student1 = Student("Oscar", "Accounting", 3.1)
 student2 =  Student("Evelyne", "Software Enginnering", 4.5)
 student3 = Student("Rose", "Electrician", 4.1)
 student5 = Student("Eva", "Software Engineer", 3.1)
-student6 = Student("Rose", "Lab medicine")
+student6 = Student("Rose", "Lab medicine", 5.1)
 
 print(student1.on_honor_role(), student2.on_honor_role(), student3.on_honor_role()) 
