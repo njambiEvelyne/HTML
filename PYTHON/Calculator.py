f9197165d2c209e076c5e18d30d4f3e9b3e1abd9
njambiEvelyne <evelynenganga76@gmail.com>
2026-09-24 15:34:17 +0300

    Add responsive navigation

--- PYTHON/Calculator.py
+++ PYTHON/Calculator.py
@@ -14,6 +14,7 @@ def add():
   print("This is addition")
   num1 = int(input("Enter the first number: "))
   num2 = int(input("Enter the second number: "))
+  
   result = num1 + num2
   print(result)
 
