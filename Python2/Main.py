ec6b002b5adffd2e6cdca1403cee9fd6cb860fbb
njambiEvelyne <evelynenganga76@gmail.com>
2026-09-25 11:41:11 +0300

    Optimize Python code

--- Python2/Main.py
+++ Python2/Main.py
@@ -24,7 +24,7 @@ def calculator():
     while True:
         choice = input("\nEnter choice (1/2/3/4/5): ")
         
-        if choice == '5':
+        if choice == '5'
             print("Thank you for using the calculator!")
             break
         
