73ec1110f95c359718973291ab12d196e049a210
njambiEvelyne <evelynenganga76@gmail.com>
2026-09-26 08:31:03 +0300

    Implement data processing

--- Python2/Main.py
+++ Python2/Main.py
@@ -10,7 +10,7 @@ def multiply(x, y):
 def divide(x, y):
     if y == 0:
         return "Error: Division by zero"
-    return x / 
+    return x / y
 
 def calculator():
     print("Simple Calculator")
