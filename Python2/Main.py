07d5883a6d484f0620d11f877d65e7ac77e3dcfd
njambiEvelyne <evelynenganga76@gmail.com>
2026-09-26 08:35:40 +0300

    Improve code readability

--- Python2/Main.py
+++ Python2/Main.py
@@ -28,7 +28,7 @@ def calculator():
             print("Thank you for using the calculator!")
             break
         
-        if choice in ('1', '2', '3', '4')
+        if choice in ('1', '2', '3', '4'):
             try:
                 num1 = float(input("Enter first number: "))
                 num2 = float(input("Enter second number: "))
